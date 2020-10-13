d9fc41704c987e16b8e402eadfebbd916de680a9
Brenno <fr.brenno@gmail.com>
2020-10-13 21:54:58 +0200

    Added permission class, authentication and profiles serching feature

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 
 class HellpApiView(APIView):
@@ -95,3 +98,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfiles,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
